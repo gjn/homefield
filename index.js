ee3ebc294b989053aeb1588eb69eae21e43b3c19
Gilbert Jeiziner <gjn@space.ch>
2014-04-30 14:22:17 +0200

    Refactor launcher to do what it's supposed to do

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 var fs = require('fs');
 
 var lib = {
-    parsers: require('./lib/parsers'),
     launcher: require('./lib/launcher')
 };
 
 module.exports = lib;
-
-function parseFile(filename, cbobject) {
-    'use strict';
-    lib.parsers().parse(filename, cbobject.addGame.bind(cbobject), cbobject.analyse.bind(cbobject));
-}
 
 var launcher = lib.launcher();
 
 if (fs.existsSync(process.argv[2]) === false) {
     console.log('No file provided or it does not exists.');
 } else {
-    parseFile(process.argv[2], launcher);
+    launcher.start(process.argv[2]);
 }
 
 
